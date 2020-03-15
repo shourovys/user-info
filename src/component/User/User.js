a4359e95de70fb0fdd28ddbdc47ee769ef6ef4fb
Shourov Saha <59758388+shourovys@users.noreply.github.com>
2020-03-15 11:41:25 +0600

    Update User.js
    add function

--- src/component/User/User.js
+++ src/component/User/User.js
@@ -4,9 +4,12 @@ import { userInfo } from 'os';
 const User = () => {
     fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
-  .then(userInfo => console.log(userInfo))
-
-  const [user,setUser]= useState(userInfo);
+  .then(userInfo => userData(userInfo))
+    
+    const userData = (userInfo)=> {
+        userInfo
+    };
+  const [user,setUser]= useState(userData);
     return (
         <div>
             <h3>Name : {user.name}</h3>
